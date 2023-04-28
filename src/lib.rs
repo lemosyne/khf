4d7177385b692d0063b6a767ced2c33f39896242
Eugene Chou <euchou@ucsc.edu>
2023-04-27 22:35:29 -0700

    add result type

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,12 @@ pub(crate) mod aliases;
 pub(crate) mod node;
 pub(crate) mod topology;
 
-mod error;
-pub use error::Error;
-
 mod khf;
 pub use crate::khf::Khf;
 
 mod kht;
 pub use crate::kht::Kht;
+
+mod error;
+pub use error::Error;
+pub type Result<T> = std::result::Result<T, Error>;
